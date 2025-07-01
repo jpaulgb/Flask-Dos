2fff66af144917278d7b221a2028d11ebae260b0
jpaulgb <jepgb@hotmail.com>
2025-06-30 22:03:47 -0500

    add html

--- app/app.py
+++ app/app.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 #vista llamada index
 def index():
-    return "hola mundo"
+    return "<h1>hola mundo_cuatro</h1>"
 
 #comprobamos si estamos en el archivo main
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True, port=5000)
 #entonces vamos a iniciar 
+#agregamos debug=True para que el SERVER se actualice
